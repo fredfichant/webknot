eec193d77d97dbddfb2e6f369d37a701f7330b98
fred fichant <fredfichant@gmail.com>
2022-03-21 14:04:20 +0100

    deployed without SSR

--- src/app/modules/application/constructions/00-main/items.ts
+++ src/app/modules/application/constructions/00-main/items.ts
@@ -6,10 +6,16 @@ export const ITEMS: any[] = [
     icon: 'far fa-address-card',
   },
   { id: 2, name: 'C9 R1', link: 'table9', icon: 'fas fa-user' },
+  { id: 3, name: 'C10', link: 'table10', icon: 'fas fa-user' },
+  { id: 4, name: 'C11', link: 'tabl11', icon: 'fas fa-user' },
+  { id: 5, name: 'C12', link: 'table12', icon: 'fas fa-user' },
+  { id: 6, name: 'C13', link: 'table13', icon: 'fas fa-user' },
+  { id: 7, name: 'C14', link: 'table14', icon: 'fas fa-user' },
+  { id: 8, name: 'C15', link: 'table15', icon: 'fas fa-user' },
   {
-    id: 3,
+    id: 9,
     name: "jacob's ladder",
-    link: 'jacob2',
+    link: '',
     icon: 'fas fa-user-friends',
   },
 ];
--- src/app/modules/application/constructions/constructions-routing.module.ts
+++ src/app/modules/application/constructions/constructions-routing.module.ts
@@ -1,9 +1,14 @@
-import { JacobComponent } from './../drawn-knots/jacob/jacob.component';
 import { Table8Component } from './table8/table8.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConstructionsComponent } from './constructions.component';
 import { Table9Component } from './table9/table9.component';
+import { VueTable10Component } from './vue-table10/vue-table10.component';
+import { VueTable11Component } from './vue-table11/vue-table11.component';
+import { VueTable12Component } from './vue-table12/vue-table12.component';
+import { VueTable13Component } from './vue-table13/vue-table13.component';
+import { VueTable14Component } from './vue-table14/vue-table14.component';
+import { VueTable15Component } from './vue-table15/vue-table15.component';
 
 const routes: Routes = [
   {
@@ -17,7 +22,12 @@ const routes: Routes = [
       },
       { path: 'table8', component: Table8Component },
       { path: 'table9', component: Table9Component },
-      { path: 'jacob2', component: JacobComponent },
+      { path: 'table10', component: VueTable10Component },
+      { path: 'table11', component: VueTable11Component },
+      { path: 'table12', component: VueTable12Component },
+      { path: 'table13', component: VueTable13Component },
+      { path: 'table14', component: VueTable14Component },
+      { path: 'table15', component: VueTable15Component },
     ],
   },
 ];
